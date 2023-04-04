bc7c22f73ac6e7c8bb4750d3f4865b8b3f5bcaf2
Christopher Snyder <snyder28139_04@hotmail.com>
2023-04-04 19:54:30 -0400

    added code to add/save/read notes

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
-const express = require('express');
+const express = require('express'); //Importing modules
 const path = require('path');
 const fs = require('fs');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const uuid = require('./helpers/uuid'); //Importing helper tp give notes unique ID
+const { readFromFile } = require('./helpers/fsUtils'); //Using readFromFile from helper
 
-const PORT = 3001;
-
+const PORT = 3001; //Sets the port and initialize the app
 const app = express();
 
-app.use(express.json());
+app.use(express.json());  //Middleware
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/index', (req, res) => { //Route for index page
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/notes", (req,res) => {
-  res.sendFile(path.join(__dirname,"/public/notes.html"));
+app.get('/notes', (req, res) => { //Route for notes page
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
+app.get('/api/notes', (req, res) => { //API route to retrieve notes
+  readFromFile(path.join(__dirname, 'db', 'db.json'))
+    .then((data) => res.json(JSON.parse(data)))
+});
 
-// FROM HERE UP IS server.js 11-19 ins
+app.post('/api/notes', (req, res) => { //API route to add a new note
+  const { title, text } = req.body;
+  const noteInput = { title, text, id: uuid() }; //uuid is unique id
 
-// HTML routes for notes.html and index.html
+      fs.readFile(path.join(__dirname, 'db', 'db.json'), (err, data) => { //Read the existing notes from the db file
+      if (err) throw err;
 
+      const dbInput = JSON.parse(data);
+      dbInput.push(noteInput);
 
-// Here Below Is #19 Also
-app.listen(PORT, () =>
-  console.log(`App App listening at http://localhost:${PORT}`)
-);
+      fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(dbInput), (err) => { // Write the new note to the db file
+        if (err) throw err;
+        console.log('Successfully added note');
+    });
+  });
+
+const response = {    // Send a response to the client
+  status: 'success',
+  body: noteInput,
+}
+  res.json(response);
+});
+
+app.listen(PORT, () => { //Listens on local server
+  console.log(`App listening at http://localhost:${PORT}`);
+});
